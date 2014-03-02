fa8151d6148a52706e8ddd036350b5a77d7934b7
unknown <code@adamcoster.com>
2014-03-02 11:12:42 -0600

    Remade perfectStage into perfectCoordinates. This script takes coordinates generated from plateCalibrator and then outputs XML coordinates for any desired subset of wells.

--- perfectCoordinates.py
+++ perfectCoordinates.py
@@ -1,98 +1,65 @@
 import re
 
 class Plate:
-    def __init__( self, inFile = '' ):
-        
-        if inFile:
-            self.readPlateDimsFromFile( inFile )
-            self.topleft_tY = float(input('Top left corner well, top Y (mm):    '))
-            self.topleft_lX = float(input('Top left corner well, left X (mm):   '))
-        else:
-            self.plateType   = input('[1] 96-well or [2] 384-well? ')
-            self.plateType   = (96, 384)[ int(self.plateType)-1 ]
-            print()
-            self.topleft_tY = float(input('Top left corner well, top Y (mm):    '))
-            self.topleft_bY = float(input('Top left corner well, bottom Y (mm): '))
-            self.topleft_lX = float(input('Top left corner well, left X (mm):   '))
-            self.topleft_rX = float(input('Top left corner well, right X (mm):  '))
-            print()
-            self.botright_bY = float(input('Bottom right corner well, bottom Y (mm): '))
-            self.botright_rX = float(input('Bottom right corner well, right X (mm):  '))
-            print()
-            self.imageWidth  = float(input('Image width (mm):  '))
-            self.imageHeight = float(input('Image Height (mm): '))
-            self.calculatePlateDims()
-            self.writePlateDimsToFile()
+    def __init__( self, inFile ):
+
+        self.readPlateDimsFromFile( inFile )
+        print('This script assumes that your plate dims file\n'+
+              'has accurate well/wall measurements.')
+
+        yesno = input('Do you need to re-calibrate the stage [y/n]? ').upper()
+        if yesno == 'Y':
+            self.B2_top  = float(input('Top  edge of B2 (mm): '))*1000
+            self.B2_left = float(input('Left edge of B2 (mm): '))*1000
+
+        self.imageWidth  = float(input('Image width  (um): '))
+        self.imageHeight = float(input('Image height (um): '))
+        self.frameGrid   = int(input('For an FxF image grid, F = '))
 
         self.makeWells()
-        
-    def writePlateDimsToFile( self, fileName = 'plateDims.info' ):
-        save = open( fileName, 'w' )
-        save.write( 'plateType='  + str(self.plateType ) +'\n')
-        save.write( 'wellWidth='  +str(self.wellWidth  ) +'\n')
-        save.write( 'wellHeight=' +str(self.wellHeight ) +'\n')
-        save.write( 'plateWidth=' +str(self.plateWidth ) +'\n')
-        save.write( 'plateHeight='+str(self.plateHeight) +'\n')
-        save.write( 'wallWidth='  +str(self.wallWidth  ) +'\n')
-        save.write( 'wallHeight=' +str(self.wallHeight ) +'\n')
-        save.write( 'imageWidth=' +str(self.imageWidth ) +'\n')
-        save.write( 'imageHeight='+str(self.imageHeight) +'\n')
-        save.close()
-        
-    def readPlateDimsFromFile( self, fileName = 'plateDims.info' ):
+
+
+    def readPlateDimsFromFile( self, fileName ):
         save = open( fileName, 'r' )
         data = save.read().strip().split()
         save.close()
-        
+
         dims = {}
         for line in data:
             key,value = line.split('=')
             dims[ key ] = float(value)
-            
+
         self.plateType  = dims['plateType'  ]
         self.wellWidth  = dims['wellWidth'  ]
         self.wellHeight = dims['wellHeight' ]
-        self.plateWidth = dims['plateWidth' ]
-        self.plateHeight= dims['plateHeight']
         self.wallWidth  = dims['wallWidth'  ]
         self.wallHeight = dims['wallHeight' ]
-        self.imageWidth = dims['imageWidth' ]
-        self.imageHeight= dims['imageHeight']    
-        
-        
-    def calculatePlateDims( self ):
-        self.wellWidth  = abs(self.topleft_lX - self.topleft_rX)
-        self.wellHeight = abs(self.topleft_tY - self.topleft_bY)
-        self.plateWidth = abs(self.topleft_lX - self.botright_rX)
-        self.plateHeight= abs(self.topleft_tY - self.botright_bY)
-        
-        self.wallWidth  = (self.plateWidth  - self.wellWidth )/(self.numCols - 1) - self.wellWidth
-        self.wallHeight = (self.plateHeight - self.wellHeight)/(self.numRows - 1) - self.wellHeight
-        
+        self.B2_left    = dims['B2_left' ]
+        self.B2_top     = dims['B2_top' ]
 
     def makeWells( self ):
-        
+
         self.cols    = self.getCols()
         self.numCols = len(self.cols)
         self.rows    = self.getRows()
-        self.numRows = len(self.rows)        
-        
-        topleft_centerX = self.topleft_lX - self.wellWidth/2
-        topleft_centerY = self.topleft_tY + self.wellHeight/2
-        
+        self.numRows = len(self.rows)
+
+        topleft_centerX = self.B2_left + self.wellWidth /2 + self.wallWidth
+        topleft_centerY = self.B2_top  - self.wellHeight/2 - self.wallHeight
+
         self.col_centers = [topleft_centerX - col*(self.wellWidth +self.wallWidth ) for col in range(self.numCols)]
         self.row_centers = [topleft_centerY + row*(self.wellHeight+self.wallHeight) for row in range(self.numRows)]
-        
+
         self.wells = []
         for row, rowCenter in zip( self.rows, self.row_centers ):
             for col, colCenter in zip( self.cols, self.col_centers):
-                well = Well( row + str(col).zfill(2), colCenter, rowCenter, self.imageWidth, self.imageHeight )
+                well = Well( row + str(col).zfill(2), colCenter, rowCenter, self.imageWidth, self.imageHeight, self.frameGrid )
                 self.wells.append(well)
-            # every other row should be flipped
+            # every other row should be flipped for snake-style imaging
             self.cols        = self.cols[::-1]
             self.col_centers = self.col_centers[::-1]
 
-                
+
     def asXML( self, wells = None ):
         self.xml = '<?xml version="1.0" encoding="UTF-16"?>'+\
                    '<variant version="1.0">'+\
@@ -106,56 +73,57 @@ class Plate:
                     pointXML = '<Point' + str(pointIdx).zfill(5) + ' runtype="NDSetupMultipointListItem">'+\
                                '<bChecked runtype="bool" value="true"/><strName runtype="CLxStringW" value="'
                     pointXML += well.name + '_' + str(frame).zfill(4) + '"/>'
-                    pointXML += '<dXPosition runtype="double" value="{:.15f}"/>'.format(well.frameX[frame]*1000)
-                    pointXML += '<dYPosition runtype="double" value="{:.15f}"/>'.format(well.frameY[frame]*1000)
+                    pointXML += '<dXPosition runtype="double" value="{:.15f}"/>'.format(well.frameX[frame])
+                    pointXML += '<dYPosition runtype="double" value="{:.15f}"/>'.format(well.frameY[frame])
                     pointXML += '<dZPosition runtype="double" value="{:.15f}"/>'.format(well.frameZ[frame])
                     pointXML += '<PFSOffset runtype="double" value="{:.15f}"/>' .format(-1.0)
                     pointXML += '</Point' + str(pointIdx).zfill(5) + '>'
-                    
+
                     self.xml += pointXML
                     pointIdx += 1
-        
+
         self.xml += '</no_name></variant>'
         return self.xml
-    
+
     def getRows( self ):
         return {96:  [chr(i) for i in range(65,73)],
                 384: [chr(i) for i in range(65,81)]}[self.plateType]
-    
+
     def getCols( self ):
         return {96:  [i+1 for i in range(12)],
                 384: [i+1 for i in range(24)]}[self.plateType]
-    
+
 class Well:
-    def __init__( self, name, centerX, centerY, imageWidth, imageHeight, z=3000 ):
+    def __init__( self, name, centerX, centerY, imageWidth, imageHeight, frameGrid, z=3000 ):
         self.name        = name
         self.centerX     = centerX
         self.centerY     = centerY
         self.Z           = z
         self.imageWidth  = imageWidth
         self.imageHeight = imageHeight
-        
-        self.generateFrameCenters()
-        
-    def generateFrameCenters( self ):
-        
-        left = self.centerX + self.imageWidth
-        top  = self.centerY - self.imageHeight
+        self.frameGrid   = frameGrid
 
-        self.frameX = [i for i in range(9)]
-        self.frameY = [i for i in range(9)]
-        self.frameZ = [i for i in range(9)]
-        
+        self.generateFrameCenters()
+
+    def generateFrameCenters( self ):
+
+        left = self.centerX + self.imageWidth *(self.frameGrid/2-.5)
+        top  = self.centerY - self.imageHeight*(self.frameGrid/2-.5)
+
+        self.frameX = [0 for i in range(self.frameGrid**2)]
+        self.frameY = [0 for i in range(self.frameGrid**2)]
+        self.frameZ = [0 for i in range(self.frameGrid**2)]
+
         frameIdx = 0
-        for frameRow in range(3):
-            for frameCol in range(3):
+        for frameRow in range(self.frameGrid):
+            for frameCol in range(self.frameGrid):
                 self.frameX[ frameIdx ] = round(left - frameCol * self.imageWidth ,15)
                 self.frameY[ frameIdx ] = round(top  + frameRow * self.imageHeight,15)
                 self.frameZ[ frameIdx ] = round(self.Z,15)
                 frameIdx += 1
-                
+
 def displayPlate( rows, cols, selection=[] ):
-    print( ' ' + ''.join([c.rjust(2) for c in cols]) ) 
+    print( ' ' + ''.join([c.rjust(2) for c in cols]) )
     for row in rows:
         line = row
         selectionsInRow = [s for s in selection if s[0] == row]
@@ -173,9 +141,9 @@ def expandWellInput( rows, cols, message ):
     rawInput = input( message ).strip().upper()
     if rawInput == 'ALL': return allWells
     if rawInput == '':    return []
-    
+
     wellSets = rawInput.split(' ')
-    
+
     # Allowed input formats are row, row-row, col, col-col,
     # rowcol, rowcol-rowcol
     # special keywords: all
@@ -183,57 +151,57 @@ def expandWellInput( rows, cols, message ):
     wellSubset = []
     for wellSet in wellSets:
         if wellSet == 'ALL': return allWells
-        
+
         # Break into row and column parts
         try:
             rowStart, colStart, rowEnd, colEnd = re.match(rowColRegex,wellSet).groups()
         except:
             input('Something was wrong with your input.')
             exit()
-        
+
 
         # Fill out the missing values
         if rowEnd   == '':
             if rowStart=='':
                 rowStart = rows[0]
-                rowEnd   = rows[-1] 
+                rowEnd   = rows[-1]
             else:
                 rowEnd = rowStart
 
         if colEnd   == '':
             if colStart=='':
                 colStart = cols[0]
-                colEnd   = cols[-1] 
+                colEnd   = cols[-1]
             else:
                 colEnd = colStart
-        
+
         subRows = [chr(r) for r in range(ord(rowStart),ord(rowEnd)+1)]
         subCols = [str(c) for c in range(int(colStart),int(colEnd)+1)]
-        
+
         subWells = [r+c for r in subRows for c in subCols if r in rows and c in cols ]
         wellSubset += subWells
-        
+
     return wellSubset
-  
 
 
- 
-    
+
+
+
 
 def main():
-    
+
     plate = Plate( input('Plate info file: ') )
     wells = expandWellInput( plate.rows, [str(c) for c in plate.cols], 'Wells to image: ' )
-    
+
     print( 'The following wells were selected: ')
     displayPlate( plate.rows, [str(c) for c in plate.cols], wells )
-    
+
     xml      = plate.asXML( [w[0]+w[1:].zfill(2) for w in wells])
     saveName = 'testOut.xml' #input('Save as: ')
     save     = open(saveName,mode='w',encoding='utf-16-le')
     save.write( xml )
     save.close()
-    
-    
+
+
 if __name__ == '__main__':
     main()
